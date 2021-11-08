37c9526e54a6a83f6ea46a2870e8e9a0c4e335f8
Chiraz588 <75127710+Chiraz588@users.noreply.github.com>
2021-11-08 09:29:48 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+  <h1 class="title red">Chiraz</h1>
+  <br/>
+  <img src="/imageInPublic.jpg" alt="img" />
+  <br/>
+  <img src="/imageInSrc.jpg" alt="img1" />
+   <div>
+   <video src="/myVideo.mp4" alt="vid" controls="controls" autoplay="true"/>
+   </div>
+
+
+    </div>
+  );
+}
+
+export default App;
